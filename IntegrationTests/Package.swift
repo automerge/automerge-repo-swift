be1ec3def098edfca1042afc9b5be23b12882920
Joe Heck <heckj@mac.com>
2024-04-07 11:19:58 -0700

    breaking integration tests off into a subpackage

---
+++ IntegrationTests/Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "IntegrationTests",
+    platforms: [.iOS(.v16), .macOS(.v13)],
+    products: [],
+    dependencies: [
+        .package(url: "https://github.com/automerge/automerge-repo-swift.git", branch: "main"),
+        // Distributed Tracing
+        // .package(url: "https://github.com/apple/swift-distributed-tracing", from: "1.0.0"),
+        // Testing Tracing
+        .package(url: "https://github.com/heckj/DistributedTracer", branch: "main"),
+        // this ^^ brings in a MASSIVE cascade of dependencies
+    ],
+    targets: [
+        .testTarget(
+            name: "AutomergeRepoIntegrationTests",
+            dependencies: [
+                .product(name: "AutomergeRepo", package: "automerge-repo-swift"),
+                .product(name: "DistributedTracer", package: "DistributedTracer"),
+            ]),
+    ]
+)
--- scripts/interop.sh
+++ scripts/interop.sh
@@ -5,14 +5,4 @@ set -eou pipefail
 THIS_SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 REPO_DIR=$THIS_SCRIPT_DIR/../
 
-cd ${REPO_DIR}
-if [ ! -d automerge-repo-sync-server ]; then
-    gh repo clone heckj/automerge-repo-sync-server
-fi
-
-cd automerge-repo-sync-server
-git fetch --all --prune
-git reset --hard origin/sync_debug
-yarn install
-NODE_ENV=dev DEBUG=* node ./src/index.js
-
+docker run -d --name syncserver -p 3030:3030 ghcr.io/heckj/automerge-repo-sync-server:main
