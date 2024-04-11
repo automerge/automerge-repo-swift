ef6c7d46242cc63da4fd4ede5e101066517b8d42
Joe Heck <heckj@mac.com>
2024-04-11 12:19:33 -0700

    correcting for usage

--- Sources/AutomergeRepo/Sync/SyncV1Msg+Errors.swift
+++ Sources/AutomergeRepo/Sync/SyncV1Msg+Errors.swift
@@ -4,22 +4,28 @@ public extension SyncV1Msg {
     enum Errors: Sendable {
         public struct Timeout: Sendable, LocalizedError {
             public var errorDescription: String = "Task timed out before completion"
-            public init(errorDescription: String) {
-                self.errorDescription = errorDescription
+            public init(errorDescription: String? = nil) {
+                if let errorDescription {
+                    self.errorDescription = errorDescription
+                }
             }
         }
 
         public struct SyncComplete: Sendable, LocalizedError {
             public var errorDescription: String = "The synchronization process is complete"
-            public init(errorDescription: String) {
-                self.errorDescription = errorDescription
+            public init(errorDescription: String? = nil) {
+                if let errorDescription {
+                    self.errorDescription = errorDescription
+                }
             }
         }
 
         public struct ConnectionClosed: Sendable, LocalizedError {
             public var errorDescription: String = "The websocket task was closed and/or nil"
-            public init(errorDescription: String) {
-                self.errorDescription = errorDescription
+            public init(errorDescription: String? = nil) {
+                if let errorDescription {
+                    self.errorDescription = errorDescription
+                }
             }
         }
 
