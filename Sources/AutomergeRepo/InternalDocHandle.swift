7a1a86d97a4dd3f1f9939f69e4c2634afcc7f652
Joe Heck <heckj@mac.com>
2024-04-22 12:24:59 -0700

    removing/commenting out debugging burr

--- Sources/AutomergeRepo/InternalDocHandle.swift
+++ Sources/AutomergeRepo/InternalDocHandle.swift
@@ -2,7 +2,6 @@ import struct Automerge.ChangeHash
 import class Automerge.Document
 import struct Automerge.SyncState
 import struct Foundation.Data
-import OSLog
 
 final class InternalDocHandle {
     enum DocHandleState {
@@ -38,14 +37,14 @@ final class InternalDocHandle {
 
     let id: DocumentId
     var doc: Automerge.Document?
-    var state: DocHandleState {
-        willSet {
-            if newValue == .unavailable {
-                print("X")
-            }
-            Logger.repo.trace("updating state of \(self.id) to \(String(describing: newValue))")
-        }
-    }
+    var state: DocHandleState
+// Uncomment for a trace/debugging point to see what's updating the state of a DocHandle, useful for setting
+// and capturing with a breakpoint...
+//    {
+//        willSet {
+//            Logger.repo.trace("updating state of \(self.id) to \(String(describing: newValue))")
+//        }
+//    }
     var remoteHeads: [STORAGE_ID: Set<Automerge.ChangeHash>]
     var syncStates: [PEER_ID: SyncState]
 
