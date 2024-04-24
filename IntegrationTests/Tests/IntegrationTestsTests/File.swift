9db13e95d88c692543ad425e8ff017cbc4682657
Joe Heck <heckj@mac.com>
2024-04-24 10:29:27 -0700

    testing doc replication on connect

---
+++ IntegrationTests/Tests/IntegrationTestsTests/File.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Joseph Heck on 4/24/24.
+//
+
+import Foundation
+import OSLog
+
+extension Logger {
+    /// Using your bundle identifier is a great way to ensure a unique identifier.
+    private static let subsystem = Bundle.main.bundleIdentifier!
+    
+    /// Logs updates and interaction related to watching for external peer systems.
+    static let test = Logger(subsystem: subsystem, category: "IntegrationTest")
+}
